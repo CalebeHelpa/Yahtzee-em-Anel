33fc8c95f2738b4072a17368b04b817a34866f55
Calebe <calebehelpa@gmail.com>
2022-08-22 22:08:49 -0300

    Adiciona funcoes restantes do game

--- game.c
+++ game.c
@@ -1,3 +1,4 @@
+
 #include "game.h"
 
 char* itos_bet(int bet){
@@ -28,9 +29,6 @@ char* itos_bet(int bet){
 void initialize_player(player_t *player, int playerId){
     player->playerID = playerId;
     player->nxtPlayerID = playerId == (TOTALPLAYERS-1) ? 0 : playerId + 1;
-
-    if(player->playerID == 0)
-        player->baton = 1;
 
     for(int i = 0; i <= (TOTALPLAYERS-1); i++)
         player->playersChips[i] = INITCHIPS;
@@ -72,11 +70,12 @@ int define_betValue(player_t *player, int betType, int betValue){
     player->betValue = 0;
 
     fprintf(stdout, "A aposta atual é: %s pelo valor %i\n", itos_bet(betType), betValue);
+    fprintf(stdout, "O seu saldo é: %i\n", player->playersChips[player->playerID]);
     fprintf(stdout, "Digite o valor que deseja apostar, caso nao queira digite um valor menor que a aposta atual\n");
     
     int value;
     scanf("%d", &value);
-    if(value <= betValue){
+    if(value <= betValue || value > player->playersChips[player->playerID]){
         fprintf(stdout, "Valor da aposta mantido, passando para o proximo jogador...\n");
         return 0;
     }
@@ -86,37 +85,266 @@ int define_betValue(player_t *player, int betType, int betValue){
     return 1;
 }
 
-void throw_dices(int **dices){
-    srand(time(NULL));
-    for(int i = 0; i < DICES; i++)
-        if(dices[0][i] == 0)
-            dices[1][i] = (rand() % 5) + 1;
+void show_bloqDices(int dices[1][NUMDICES-1]){
+    fprintf(stdout, "Os dados bloqueados sao:\n");
+    for(int i = 0; i < NUMDICES; i++)
+        if(dices[0][i] == 1)
+            fprintf(stdout, "%d ", dices[1][i]);
+    fprintf(stdout, "\n");
 }
 
-void freeze_dices(int **dices){
-    for(int i = 0; i < DICES; i++)
+void show_dices(int dices[1][NUMDICES-1]){
+    fprintf(stdout, "Os resultados sao:\n");
+    for(int i = 0; i < NUMDICES; i++)
         fprintf(stdout, "%d ", dices[1][i]);
     fprintf(stdout, "\n");
+}
 
+void throw_dices(int dices[1][NUMDICES-1]){
+    fprintf(stdout, "Jogando os dados...\n");
+    srand(time(NULL));
+    for(int i = 0; i < NUMDICES; i++)
+        if(dices[0][i] == 0)
+            dices[1][i] = (rand() % 5) + 1;
+    show_dices(dices);
+}
+
+void freeze_dices(int dices[1][NUMDICES-1]){
+    fprintf(stdout, "Insira uma sequencia de zeros e uns, sem espaços, para os dados que deseja bloquear, respectivamente\n");
+    fprintf(stdout, "(Um para bloquear, zero para rolar novamente)\n");
+    fprintf(stdout, ">>> Atencao! Uma vez o dado bloqueado ele nao pode ser desbloqueado!\n");
+
+    int buff;
+    for(int i = 0; i < NUMDICES; i++){
+        buff = getc(stdin) - 48;
+        if(dices[0][i] == 0)
+            dices[0][i] = buff;
+    }
+    getc(stdin);
+
+    show_bloqDices(dices);
+
+}
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+ 
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+ 
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < n - 1; i++) {
+ 
+        // Find the minimum element in unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+ 
+        // Swap the found minimum element
+        // with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+int check_par(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int find = 0;
+    while(i < NUMDICES-1 && j < NUMDICES && find == 0){
+        if(dices[1][i] == dices[1][j])
+            find = 1;
+        i++;
+        j++;
+    }
+    if(!find)
+        return 0;
     
+    player->playersChips[player->playerID] += RECPAR;
+    return 1;
+}
+
+int check_trio(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int k = 2;
+    int find = 0;
+    while(i < NUMDICES-2 && j < NUMDICES-1 && k < NUMDICES && find == 0){
+        if((dices[1][i] == dices[1][j]) && (dices[1][j] == dices[1][k]))
+            find = 1;
+        i++;
+        j++;
+        k++;
+    }
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECTRIO;
+    return 1;
+}
+
+int check_doispares(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int k = 2;
+    int l = 3;
+    int find = 0;
+    while(i < NUMDICES-3 && j < NUMDICES-2 && k < NUMDICES-1 && l < NUMDICES && find == 0){
+        if((dices[1][i] == dices[1][j]) && (dices[1][k] == dices[1][l]))
+            find = 1;
+        i++;
+        j++;
+        k++;
+        l++;
+    }
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECDOISPARES;
+    return 1;
+}
+
+int check_fullhouse(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int k = 2;
+    int l = 3;
+    int m = 4;
+    int find = 0;
+
+    if((dices[1][i] == dices[1][j]) && ((dices[1][k] == dices[1][l]) && (dices[1][l] == dices[1][m])))
+        find = 1;
+    else if ((dices[1][l] == dices[1][m]) && ((dices[1][i] == dices[1][j]) && (dices[1][j] == dices[1][k])))
+        find = 1;
+
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECFULLHOUSE;
+    return 1;
+}
+
+int check_seqbaixa(player_t *player, int dices[1][NUMDICES-1]){
+    int find = 0;
+    if((dices[1][0] == 1) && (dices[1][1] == 2) && (dices[1][2] == 3) && (dices[1][3] == 4) && (dices[1][4] == 5))
+        find = 1;
+
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECSEQBAIXA;
+    return 1;
+}
+
+int check_seqalta(player_t *player, int dices[1][NUMDICES-1]){
+    int find = 0;
+    if((dices[1][0] == 2) && (dices[1][1] == 3) && (dices[1][2] == 4) && (dices[1][3] == 5) && (dices[1][4] == 6))
+        find = 1;
+
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECSEQALTA;
+    return 1;
+}
+
+int check_quadra(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int k = 2;
+    int l = 3;
+    int find = 0;
+    while(i < NUMDICES-3 && j < NUMDICES-2 && k < NUMDICES-1 && l < NUMDICES && find == 0){
+        if((dices[1][i] == dices[1][j]) && (dices[1][j] == dices[1][k]) && (dices[1][k] == dices[1][l]))
+            find = 1;
+        i++;
+        j++;
+        k++;
+        l++;
+    }
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECQUADRA;
+    return 1;
+}
+
+int check_quinteto(player_t *player, int dices[1][NUMDICES-1]){
+    int i = 0;
+    int j = 1;
+    int k = 2;
+    int l = 3;
+    int m = 4;
+    int find = 0;
+    while(i < NUMDICES-4 && j < NUMDICES-3 && k < NUMDICES-2 && l < NUMDICES-1 && m < NUMDICES && find == 0){
+        if((dices[1][i] == dices[1][j]) && (dices[1][j] == dices[1][k]) && (dices[1][k] == dices[1][l]) && (dices[1][l] == dices[1][m]))
+            find = 1;
+        i++;
+        j++;
+        k++;
+        l++;
+        m++;
+    }
+    if(!find)
+        return 0;
+    
+    player->playersChips[player->playerID] += RECQUINTETO;
+    return 1;
+}
+
+int define_result(player_t *player, int dices[1][NUMDICES-1]){
+    selectionSort(dices[1], NUMDICES);
+    switch(player->betType){
+    case PAR:
+        return check_par(player, dices);
+    case TRIO:
+        return check_trio(player, dices);
+    case DOISPARES:
+        return check_doispares(player, dices);
+    case FULLHOUSE:
+        return check_fullhouse(player, dices);
+    case SEQBAIXA:
+        return check_seqbaixa(player, dices);
+    case SEQALTA:
+        return check_seqalta(player, dices);
+    case QUADRA:
+        return check_quadra(player, dices);
+    case QUINTETO:
+        return check_quinteto(player, dices);
+    default:
+        return(-1);
+        break;
+    }
 }
 
 int make_play(player_t *player){
     
+    fprintf(stdout, "Pressione qualquer tecla para jogar os dados\n");
+    getc(stdin);
+
     player->playersChips[player->playerID] -= player->betValue;
 
-    int dices[1][DICES-1];
-    for(int i = 0; i < DICES; i++){
+    int dices[1][NUMDICES-1];
+    for(int i = 0; i < NUMDICES; i++){
         dices[0][i] = 0;
         dices[1][i] = 0;
     }
-    
 
-    int numPlays;
-    for(numPlays = 0; numPlays < 3; numPlays++){ 
+    int numPlays = 0;
+    int playerStop = 0;
+    while(numPlays < 3 && playerStop == 0){
         throw_dices(dices);
         freeze_dices(dices);
+        fprintf(stdout, "Se deseja jogar os dados novamente pressione 1, senao pressione 0\n");
+        scanf("%d", &playerStop);
+        numPlays++;
     }
-    
-    
+
+    return define_result(player, dices);
 }
--- game.h
+++ game.h
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define INITCHIPS 10
 #define TOTALPLAYERS 4
-#define DICES 5
+#define NUMDICES 5
 
 #define NULO -1
 #define PAR 1
@@ -19,11 +20,19 @@
 #define QUADRA 7
 #define QUINTETO 8
 
+#define RECPAR 2
+#define RECTRIO 3
+#define RECDOISPARES 4
+#define RECFULLHOUSE 5
+#define RECSEQBAIXA 7
+#define RECSEQALTA 7
+#define RECQUADRA 10
+#define RECQUINTETO 15
+
 typedef struct player_t {
   int playerID;                     // ID do jogador
   int nxtPlayerID;                  // ID do proximo jogador
 
-  int baton;                        // 1 se possui o bastao, 0 caso contrario
   int playersChips[TOTALPLAYERS-1]; // Vetor para monitorar fichas dos jogadores atraves do ID 
   
   int betType;                      // Tipo da aposta
